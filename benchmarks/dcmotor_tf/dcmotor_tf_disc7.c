284945893b77fb02efe574c2a0946e7882c173d6
Lucas Cordeiro <lucasccordeiro@gmail.com>
2016-12-05 14:19:51 +0000

    DC Motor
    Feedback Control of Dynamics Systems, 6th ed, Franklin, G. F.; Powell, J. D.; Emami-Naemi

---
+++ benchmarks/dcmotor_tf/dcmotor_tf_disc7.c
@@ -0,0 +1,25 @@
+#include <dsverifier.h>
+
+digital_system controller = {
+        .b = { 0.2f, -0.18f },
+        .b_size = 2,
+        .a = { 1.0f, 0.3f },
+        .a_size = 2,
+        .sample_time = 2
+};
+
+implementation impl = {
+        .int_bits = 3,
+        .frac_bits = 7,
+        .max = 1.0,
+        .min = -1.0,
+        .scale = 1
+};
+
+digital_system plant = {
+        .b = { 0.0018f, 1.9971E-4},
+        .b_size = 2,
+        .a = { 1.0f, -0.9990f, 4.5355E-5},
+        .a_size = 3
+};
+
