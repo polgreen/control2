95335d5204380a2f1cb161040800582e05d1fa9d
Pascal Kesseli <pascal_kesseli@hotmail.com>
2017-01-18 17:39:49 +0000

    Implemented K size check.

--- benchmark-runner/accelerator-ss-benchmark-runner.sh
+++ benchmark-runner/accelerator-ss-benchmark-runner.sh
@@ -2,6 +2,13 @@
 export PATH=${PATH//cbmc-5190/cbmc-trunk-diffblue-control-synthesis}
 export PATH=${PATH}:/users/pkesseli/software/cpp/cbmc/cbmc-trunk-diffblue-control-synthesis/src/cegis:/users/pkesseli/software/cpp/cbmc/cbmc-trunk-diffblue-control-synthesis-analyzer/src/goto-analyzer:/users/pkesseli/software/cpp/z3/trunk/target/i686-linux/bin
 
+status_output_file='output.txt'
+synthesis_file='FWL_LTI.c'
+width_check_file='width_LTI.c'
+working_directory_base_suffix='-dkr10'
+working_directory_base="/tmp/control_synthesis-ss${working_directory_base_suffix}"
+spec_header_file='spec.h'
+
 function setup_benchmark_directory {
  mkdir -p "$1" 2>/dev/null
  # TODO: cp source files and headers, and benchmark headers
@@ -35,15 +42,35 @@ function extract_frac_bits {
  echo $1 | sed "s/.*implementation *< *[^,]*,\([^> ]*\) *>.*/\1/"
 }
 
-status_output_file='output.txt'
-synthesis_file='FWL_LTI.c'
-width_check_file='width_LTI.c'
-working_directory_base_suffix='-dkr10'
-working_directory_base="/tmp/control_synthesis-ss${working_directory_base_suffix}"
+function extract_input_lower_bound {
+ echo $1 | sed "s/.*inputs *= *\[ *\([^,]*\) *,.*/\1/"
+}
+
+function extract_input_upper_bound {
+ echo $1 | sed "s/.*inputs *= *\[ *[^,]*,\([^]]*\).*/\1/"
+}
+
+function write_spec_header {
+ echo "#define NSTATES ${num_states}" >${spec_header_file}
+ echo "#define NINPUTS ${num_inputs}" >>${spec_header_file}
+ echo "#define NOUTPUTS ${num_outputs}" >>${spec_header_file}
+ controller_fixedbv_type_width=$((impl_int_bits+impl_frac_bits))
+ width_offset=$((controller_fixedbv_type_width%8))
+ [ ${width_offset} -ne 0 ] && impl_int_bits=$((impl_int_bits+8-width_offset))
+ echo "#define INT_BITS ${impl_int_bits}" >>${spec_header_file}
+ echo "#define FRAC_BITS ${impl_frac_bits}" >>${spec_header_file}
+ A_value=$(echo $1 | sed -r 's/;/ }, { /g')
+ echo "const __CPROVER_EIGEN_fixedbvt _controller_A[NSTATES][NSTATES] = { { ${A_value} } };" >>${spec_header_file}
+ B_value=$(echo $2 | sed -r 's/;/, /g')
+ echo "const __CPROVER_EIGEN_fixedbvt _controller_B[NSTATES] = { ${B_value} };" >>${spec_header_file}
+ echo "#define INPUT_LOWERBOUND (__CPROVER_EIGEN_fixedbvt)$3" >>${spec_header_file}
+ echo "#define INPUT_UPPERBOUND (__CPROVER_EIGEN_fixedbvt)$4" >>${spec_header_file}
+}
+
 mkdir -p ${working_directory_base} 2>/dev/null
 
 if [ -z "$1" ]; then
- benchmark_dirs=('/users/pkesseli/documents/control-synthesis/benchmarks/dcmotor_ss/')
+ benchmark_dirs=('/users/pkesseli/documents/control-synthesis/benchmarks/state-space/dcmotor_ss/')
 else
  benchmark_dirs=("$1")
 fi
@@ -64,9 +91,9 @@ for benchmark_dir in ${benchmark_dirs[@]}; do
   num_outputs=$(extract_spec_scalar "${spec_content}" 'outputs')
   A=$(extract_spec_matrix "${spec_content}" 'A')
   B=$(extract_spec_matrix "${spec_content}" 'B')
-  C=$(extract_spec_matrix "${spec_content}" 'C')
-  D=$(extract_spec_matrix "${spec_content}" 'D')
-  inputs=$(extract_spec_matrix "${spec_content}" 'inputs')
+  input_lower_bound=$(extract_input_lower_bound "${spec_content}")
+  input_upper_bound=$(extract_input_upper_bound "${spec_content}")
+  write_spec_header "$A" "$B" "$input_lower_bound" "$input_upper_bound"
   exit 3
 
   working_directory="${working_directory_base}/accelerator-ss"
--- benchmark-runner/discrete_step_k_completeness_check.cpp
+++ benchmark-runner/discrete_step_k_completeness_check.cpp
@@ -1,17 +1,25 @@
 #include <Eigen/Eigenvalues>
 #include <eigen3/unsupported/Eigen/MPRealSupport>
 #include <mpfr.h>
+#include <iostream>
+
+typedef mpfr::mpreal realt;
+typedef realt __CPROVER_EIGEN_fixedbvt;
+#include "spec.h"
 
 using namespace Eigen;
 using namespace std;
 using namespace mpfr;
 
-typedef mpreal realt;
-typedef Matrix<realt, Dynamic, Dynamic> matrixt;
-typedef Matrix<realt, Dynamic, 1> vectort;
-
 #define EXIT_INCREASE_K 2
-#define PRECISION 256u
+#define PRECISION 256
+#define K_SIZE_ARG_INDEX 1u
+
+typedef Matrix<realt, NSTATES, NSTATES> matrixt;
+
+// TODO: Read actual K
+//const realt _controller_K[] = { 0.0234375,-0.1328125, 0.00390625 };
+const realt _controller_K[] = { 0.0234375,0.1328125, 0.00390625 };
 
 int main(const int argc, const char * const argv[]) {
   const realt two = "2.0";
@@ -19,22 +27,39 @@ int main(const int argc, const char * const argv[]) {
   const realt imaginary_offset = pow(two, -PRECISION/4);
   if (argc < 2) return EXIT_FAILURE;
   mpreal::set_default_prec(PRECISION);
-  // TODO: Read A, B, K, K_SIZE
-  matrixt A;
-  matrixt B;
-  vectort K;
-  const realt K_SIZE = "0.0";
+  const realt K_SIZE = argv[K_SIZE_ARG_INDEX];
+  Matrix<realt, NSTATES, NSTATES> A;
+  for (size_t i = 0; i < NSTATES; ++i) {
+    for (size_t j = 0; j < NSTATES; ++j) {
+      A(i, j) = _controller_A[i][j];
+    }
+  }
+  Matrix<realt, NSTATES, 1> B;
+  for (size_t i = 0; i < NSTATES; ++i) {
+    B(i) = _controller_B[i];
+  }
+  Matrix<realt, 1, NSTATES> K;
+  for (size_t i = 0; i < NSTATES; ++i) {
+    K[i] = _controller_K[i];
+  }
   matrixt result = A - B * K;
   EigenSolver<matrixt> eigenSpace(result);
   if (Success != eigenSpace.info())
     return EXIT_FAILURE;
   const EigenSolver<matrixt>::EigenvalueType eigenvalues = eigenSpace.eigenvalues();
+  cout << "num_eigenvalues: " << eigenvalues.size() << endl;
   for (size_t i=0; i < eigenvalues.size(); ++i) {
+    cout << "eigenvalue: " << eigenvalues[i] << endl;
     const realt imag_value = std::imag(eigenvalues[i]);
-    if (imag_value <= imaginary_offset) continue;
+    cout << "imag_value: " << imag_value << endl;
+    cout << "imaginary_offset: " << imaginary_offset << endl;
+    if (abs(imag_value) <= imaginary_offset) continue;
     const realt angle = std::arg(eigenvalues[i]);
-    const realt expected = two_pi / angle;
-    if (expected > K_SIZE) return EXIT_INCREASE_K;
+    const realt expected = abs(two_pi / angle);
+    cout << "expected_k_size=" << expected << endl;
+    cout << "actual_k_size=" << K_SIZE << endl;
+    if (expected > K_SIZE)
+      return EXIT_INCREASE_K;
   }
   return EXIT_SUCCESS;
 }
