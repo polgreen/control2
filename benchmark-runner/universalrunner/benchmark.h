124b915cf822d645709675bc63ce544aa2a8da2c
polgreen <elizabeth.polgreen@cs.ox.ac.uk>
2017-01-19 18:11:33 +0000

    integrating intervals: THIS IS BROKEN

--- benchmark-runner/universalrunner/benchmark.h
+++ benchmark-runner/universalrunner/benchmark.h
@@ -15,8 +15,8 @@
 #define NSTATES 3
 #define NINPUTS 1
 #define NOUTPUTS 1
-#define INPUT_UPPERBOUND (__controller_typet)1
-#define INPUT_LOWERBOUND (__controller_typet)-1
+#define INPUT_UPPERBOUND (__plant_precisiont)1
+#define INPUT_LOWERBOUND (__plant_precisiont)-1
 const __plant_typet _controller_A[NSTATES][NSTATES] = {{ 0.9905,0.075687,0.021033 }, { 0.125,0,0 }, { 0,0.015625,0 }};
 const __plant_typet _controller_B[NSTATES] = { 16, 0, 0 };
 
