d768b57ebcf15dcdd9122f10ed46951ca844e725
polgreen <elizabeth.polgreen@cs.ox.ac.uk>
2017-01-20 14:00:05 +0000

    fixing merge in controltypes.h

--- benchmark-runner/universalrunner/control_types.h
+++ benchmark-runner/universalrunner/control_types.h
@@ -8,13 +8,15 @@
 #ifndef CONTROL_TYPES_H_
 #define CONTROL_TYPES_H_
 
+
 #ifdef CPROVER
     #ifdef INTERVAL
-      #include "intervals.h"
       typedef   __CPROVER_fixedbv[24][12] __plant_precisiont;
+      #include "intervals.h"
       typedef   struct intervalt __plant_typet;
       typedef   __CPROVER_fixedbv[24][12] __controller_precisiont;
       typedef   struct intervalt __controller_typet;
+      #define interval(x) interval_cast(x)
     #else
       typedef __CPROVER_fixedbv[24][12] __plant_precisiont;
       typedef __plant_precisiont __plant_typet;
@@ -23,19 +25,17 @@
         #define plant_cast(x) ((__plant_typet)x)
         #define controller_cast(x) ((__controller_typet)x)
     #endif
-/*#else
-  #include "fixedpoint.h"
-  #ifdef INTERVAL
+#else
+     #ifndef INTERVAL
+     #define INTERVAL
+     #endif
      typedef double __plant_precisiont;
-     typedef intervalt __plant_typet;
-     typedef fxp_t __controller_precisiont;
-     typedef intervalt __controller_typet;
-   #else
-     typedef double __plant_typet;
-     typedef fxp_t __controller_typet;
-      #define plant_cast(x) ((__plant_typet)x)
-      #define controller_cast(x) ((__controller_typet)x)
-   #endif*/
+      #include "intervals.h"
+     typedef struct intervalt __plant_typet;
+     typedef double __controller_precisiont; //fixed point arithmetic is implemented using doubles
+     typedef struct intervalt __controller_typet;
+     #define interval(x) interval_cast(x)
+
 #endif
 
 #endif /* CONTROL_TYPES_H_ */
