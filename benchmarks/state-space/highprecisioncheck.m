7838b2dc2e18109662d3730188e01d6bcb1ea29b
polgreen <elizabeth.polgreen@cs.ox.ac.uk>
2017-01-25 03:26:49 +0000

    more matlab benchmark checks

--- benchmarks/state-space/highprecisioncheck.m
+++ benchmarks/state-space/highprecisioncheck.m
@@ -26,10 +26,6 @@ digits(256);
 fxp=@(v) fi(double(v),1,16,8,FiSettings);
 fxpP = @(v) vpa(v);% fi(v,0.5,64,32,FiSettingsP);
 
-msg = 'dcmotor_ss_disc1.ss' %UNSAFE
-K = [fxp(0.0078125), fxp(-0.0078125)];
-A = fxpP([ 0.90122,1.3429e-08;7.4506e-09,0 ]);
-B = fxpP([ 128;0 ]);
 
 loops = 50;
 numstates = numel(B);
@@ -49,18 +45,18 @@ end
 
   
 for j=1:2^(numstates)
- % states = statematrix(:,j) ;
- states = fxpP([-0.5;0.5]);
+  states = statematrix(:,j) ;
+ %states = fxpP([-0.5;0.5]);
   for i=1:loops
 
-    tmp = fxp(states)  
-    input = -K * fxp(states)
-    tmp_input = fxpP(input)
+
+    input = -K * fxp(states);
+
     if(input>INPUT(2) || input< INPUT(1))
         msg = 'input too big'
         return
     end    
-    states = A * (states) + B * double(input)
+    states = A * (states) + B * double(input);
     for idx = 1: numel(states)
         if(states(idx) > 1 || states(idx) < -1)
             msg = '!!!!!UNSAFE!!!!!'
