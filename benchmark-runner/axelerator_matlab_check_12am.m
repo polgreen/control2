c1d3cbbfd2d60f966e521dcd5ff28058b7b1ea3e
polgreen <elizabeth.polgreen@cs.ox.ac.uk>
2017-01-25 02:11:34 +0000

    high precision check + more matlab check results

--- benchmark-runner/axelerator_matlab_check_12am.m
+++ benchmark-runner/axelerator_matlab_check_12am.m
@@ -1,12 +1,14 @@
+%FINAL RESULTS AT 01:35
+
 msg = 'cruiseHSCC_ss_disc1.ss'
 K = [fxp(0.5)];
 A = [ 0.998 ];
 B = [ 0.125 ];
 INPUT = [-1,1];
-%run fixedpointcheck.m %single state = checked manually
+%run fixedpointcheck.m %single state, checked safe
 
-msg = 'dcmotor_ss_disc1.ss'
-K = [fxp(0), fxp(-0.96875)];
+msg = 'dcmotor_ss_disc1.ss' %UNSAFE
+K = [fxp(0.0078125), fxp(-0.0078125)];
 A = [ 0.90122,1.3429e-08;7.4506e-09,0 ];
 B = [ 128;0 ];
 INPUT = [-1,1];
@@ -17,4 +19,25 @@ K = [fxp(0.234375), fxp(-0.1484375), fxp(0.078125)];
 A = [ 2.6207,-1.1793,0.65705;2,0,0;0,0.5,0 ];
 B = [ 8;0;0 ];
 INPUT = [-1,1];
+run fixedpointcheck.m
+
+msg = 'magsuspension_ss_disc2.ss'
+K = [fxp(28.546875), fxp(-11.0234375)];
+A = [ 2.001,-1;1,0 ];
+B = [ 0.0625;0 ];
+INPUT = [-1000,1000];
+run fixedpointcheck.m
+
+msg = 'invpendulum_pendang_ss_disc1.ss'
+K = [fxp(4.75), fxp(-1)];
+A = [ 2.2553,-1;1,0 ];
+B = [ 0.5;0 ];
+INPUT = [-10,10];
+run fixedpointcheck.m
+
+msg = 'pendulum_ss_disc1.ss'
+K = [fxp(-0.61328125), fxp(-0.125)];
+A = [ -1.9999,-1;1,0 ];
+B = [ 4;0 ];
+INPUT = [-10,10];
 run fixedpointcheck.m
---
+++ benchmarks/state-space/highprecisioncheck.m
@@ -0,0 +1,75 @@
+
+global FiSettings ;
+global FiSettingsP ;
+
+FiSettings = fimath('ProductMode',...
+                   'SpecifyPrecision',... 
+                   'ProductWordLength',16,... 
+                   'ProductFractionLength',8, ...
+                   'RoundingMethod','Floor',...
+                   'SumMode', 'SpecifyPrecision',...
+                   'SumWordLength',16, ...
+                   'SumFractionLength', 8, ...
+                   'OverflowAction', 'Wrap');
+               
+ FiSettingsP = fimath('ProductMode',...
+                   'SpecifyPrecision',... 
+                   'ProductWordLength',64,... 
+                   'ProductFractionLength',32, ...
+                   'RoundingMethod','Floor',...
+                   'SumMode', 'SpecifyPrecision',...
+                   'SumWordLength',64, ...
+                   'SumFractionLength', 32, ...
+                   'OverflowAction', 'Wrap');              
+               
+digits(256);             
+fxp=@(v) fi(double(v),1,16,8,FiSettings);
+fxpP = @(v) vpa(v);% fi(v,0.5,64,32,FiSettingsP);
+
+msg = 'dcmotor_ss_disc1.ss' %UNSAFE
+K = [fxp(0.0078125), fxp(-0.0078125)];
+A = fxpP([ 0.90122,1.3429e-08;7.4506e-09,0 ]);
+B = fxpP([ 128;0 ]);
+
+loops = 2;
+numstates = numel(B);
+
+if(numstates==2)
+    statematrix = [fxpP(-0.5),fxpP(-0.5),fxpP(0.5),fxpP(0.5) ; fxpP(-0.5),fxpP(0.5),fxpP(-0.5), fxpP(0.5)];
+elseif (numstates==3)
+    statematrix = [fxpP(-0.5),fxpP(-0.5),fxpP(-0.5),fxpP(-0.5),fxpP(0.5),fxpP(0.5),fxpP(0.5),fxpP(0.5); ...
+                   fxpP(-0.5),fxpP(-0.5),fxpP(0.5), fxpP(0.5),fxpP(-0.5),fxpP(-0.5),fxpP(0.5),fxpP(0.5); ...
+                   fxpP(-0.5),fxpP(0.5),fxpP(-0.5), fxpP(0.5),fxpP(-0.5),fxpP(0.5),fxpP(-0.5),fxpP(0.5)];
+elseif (numstates==4)   
+    statematrix = [fxpP(-0.5),fxpP(-0.5),fxpP(-0.5),fxpP(-0.5),fxpP(-0.5),fxpP(-0.5),fxpP(-0.5),fxpP(-0.5),fxpP(0.5),fxpP(0.5),fxpP(0.5),fxpP(0.5),fxpP(0.5),fxpP(0.5),fxpP(0.5),fxpP(0.5); ...
+                   fxpP(-0.5),fxpP(-0.5),fxpP(-0.5),fxpP(-0.5),fxpP(0.5),fxpP(0.5),fxpP(0.5),fxpP(0.5),fxpP(-0.5),fxpP(-0.5),fxpP(-0.5),fxpP(-0.5),fxpP(0.5),fxpP(0.5),fxpP(0.5),fxpP(0.5); ...
+                   fxpP(-0.5),fxpP(-0.5),fxpP(0.5), fxpP(0.5),fxpP(-0.5),fxpP(-0.5),fxpP(0.5),fxpP(0.5),fxpP(-0.5),fxpP(-0.5),fxpP(0.5), fxpP(0.5),fxpP(-0.5),fxpP(-0.5),fxpP(0.5),fxpP(0.5); ...
+                   fxpP(-0.5),fxpP(0.5),fxpP(-0.5), fxpP(0.5),fxpP(-0.5),fxpP(0.5),fxpP(0.5),fxpP(0.5),fxpP(-0.5),fxpP(0.5),fxpP(-0.5), fxpP(0.5),fxpP(-0.5),fxpP(0.5),fxpP(0.5),fxpP(0.5)];
+end
+
+  
+for j=1:2^(numstates)
+ % states = statematrix(:,j) ;
+ states = [-0.5;0.5];
+  for i=1:loops
+
+    tmp = fxp(states)  
+    input = -K * fxp(states)
+    tmp_input = double(input);
+    if(input>INPUT(2) || input< INPUT(1))
+        msg = 'input too big'
+        return
+    end    
+    states = A * (states) + B * double(input)
+    for idx = 1: numel(states)
+        if(states(idx) > 1 || states(idx) < -1)
+            msg = '!!!!!UNSAFE!!!!!'
+            i
+            states
+            statematrix(:,j)
+            return
+        end  
+    end
+  end 
+end
+msg = 'safe and stable'
