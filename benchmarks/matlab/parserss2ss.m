09f907c24693f03c04d3ee1776f88b54615940fb
Lennon Chaves <lennon.correach@gmail.com>
2017-06-12 08:44:29 -0400

    added script for automatic ss files generation

---
+++ benchmarks/matlab/parserss2ss.m
@@ -0,0 +1,39 @@
+function parserss2ss (system, inputs, intBits, fracBits, maxrange, minrange, K, name)
+
+A = system.A;
+B = system.B;
+C = system.C;
+D = system.D;
+
+[rA,cA] = size(A);
+nStates = rA;
+[rB,cB] = size(B);
+nInputs = cB;
+[rC,cC] = size(C);
+nOutputs = rC;
+
+frac_bits = fracBits;
+int_bits = intBits;
+max_range = maxrange;
+min_range = minrange;
+
+file_name = [name '.ss'];
+
+fid = fopen(file_name, 'wt' );
+
+fprintf(fid,'implementation <%d,%d>\n', int_bits, frac_bits);
+fprintf(fid,'range [%d,%d]\n', max_range, min_range);
+fprintf(fid,'states = %d;\n', nStates);
+fprintf(fid,'inputs = %d;\n', nInputs);
+fprintf(fid,'outputs = %d;\n', nOutputs);
+fprintf(fid,'A = %s \n', matrix2string(A));
+fprintf(fid,'B = %s \n', matrix2string(B));
+fprintf(fid,'C = %s \n', matrix2string(C));
+fprintf(fid,'D = %s \n', matrix2string(D));
+fprintf(fid,'inputs = %s \n', matrix2string(inputs));
+if isempty(K) == 0
+   fprintf(fid,'K = %s \n', matrix2string(K));
+end
+fclose(fid);
+
+end
