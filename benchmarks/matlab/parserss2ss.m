9261749762c43121383e222da1e7163b4e2d903a
Lennon Chaves <lennon.correach@gmail.com>
2017-06-12 09:01:14 -0400

    updated script

--- benchmarks/matlab/parserss2ss.m
+++ benchmarks/matlab/parserss2ss.m
@@ -1,5 +1,20 @@
 function parserss2ss (system, inputs, intBits, fracBits, maxrange, minrange, K, name)
-
+% Translate the matlab systems to a ss file
+%
+% Function: parserss2ss(system, inputs, intBits, fracBits, maxrange, minrange, K, name)
+%
+%  system: the physical plant in state-space format
+%  inputs: the inputs for the state-space system
+%  intBits: integer bits implementation
+%  fracBits: fractional bits implementation
+%  maxrange: maximum dynamic range
+%  minrange: minimum dynamic range
+%  K: feedback matrix for closed-loop systems
+%  name: the name for the .ss file produced
+%
+% Author: Lennon Chaves
+% June 2017
+%
 A = system.A;
 B = system.B;
 C = system.C;
@@ -12,17 +27,12 @@ nInputs = cB;
 [rC,cC] = size(C);
 nOutputs = rC;
 
-frac_bits = fracBits;
-int_bits = intBits;
-max_range = maxrange;
-min_range = minrange;
-
 file_name = [name '.ss'];
 
 fid = fopen(file_name, 'wt' );
 
-fprintf(fid,'implementation <%d,%d>\n', int_bits, frac_bits);
-fprintf(fid,'range [%d,%d]\n', max_range, min_range);
+fprintf(fid,'implementation <%d,%d>\n', intBits, fracBits);
+fprintf(fid,'range [%d,%d]\n', maxrange, minrange);
 fprintf(fid,'states = %d;\n', nStates);
 fprintf(fid,'inputs = %d;\n', nInputs);
 fprintf(fid,'outputs = %d;\n', nOutputs);
