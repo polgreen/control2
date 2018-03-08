3056c6196b89503a6e82f11ee0269172491f8dcf
iurybessa <iury.bessa@gmail.com>
2018-03-08 10:20:36 -0400

    adding Ts info in .ss

--- benchmarks/matlab/parserss2ss.m
+++ benchmarks/matlab/parserss2ss.m
@@ -19,6 +19,7 @@ A = system.A;
 B = system.B;
 C = system.C;
 D = system.D;
+sampletime = system.Ts;
 
 [rA,cA] = size(A);
 nStates = rA;
@@ -41,6 +42,7 @@ fprintf(fid,'B = %s \n', matrix2string(B));
 fprintf(fid,'C = %s \n', matrix2string(C));
 fprintf(fid,'D = %s \n', matrix2string(D));
 fprintf(fid,'inputs = %s \n', matrix2string(inputs));
+fprintf(fid,'Ts = %s \n', num2str(sampletime));
 if isempty(K) == 0
    fprintf(fid,'K = %s \n', matrix2string(K));
 end
