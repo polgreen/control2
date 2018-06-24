148633446280d21541a0db5cc9d470712e056d31
Lucas Cordeiro <lucasccordeiro@gmail.com>
2018-06-24 19:11:16 +0100

    keep the suspension benchmark with 1000000 as input
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- benchmarks/matlab/get_inputs.m
+++ benchmarks/matlab/get_inputs.m
@@ -5,7 +5,12 @@ INPUT_100 = {'magneticpointer', 'invpendulum_cartpos','steamdrum','cstrtemp'};
 INPUT_1000 = {'magsuspension'};
 INPUT_1000000 = {'suspension'};
 
-INPUT=[-10000,10000];
+if (getnameidx(INPUT_1000000,Name)~=0 )
+     INPUT=[-1000000,1000000];
+else
+     INPUT=[-10000,10000];
+end
+
 %     if(getnameidx(INPUT_10,Name)~=0)
 %         INPUT=[-10,10];
 %     elseif (getnameidx(INPUT_100,Name)~=0 )
