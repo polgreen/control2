16f4ed61bfc2d0b2757decc83941cb4185ea7fb8
polgreen <elizabeth.polgreen@cs.ox.ac.uk>
2018-10-10 10:06:30 +0100

    benchmark switch

--- benchmark-runner/universalrunner/benchmark.h
+++ benchmark-runner/universalrunner/benchmark.h
@@ -1,28 +1,25 @@
-/*
- * benchmark.h
- *
- *  Created on: 18 Jan 2017
- *      Author: elipol
- */
+#ifndef BENCHMARK_H_ 
+#define BENCHMARK_H_ 
 
-#ifndef BENCHMARK_H_
-#define BENCHMARK_H_
-#define INTERVAL 1
-#ifndef INT_BITS
-  #define INT_BITS 6
-  #define FRAC_BITS 10
+// time discretisation 0.050  
+#ifndef INT_BITS 
+#define INT_BITS 32
+#define FRAC_BITS 32
 #endif
-#define NSTATES 2
+#define NSTATES 3 
 #include "control_types.h"
-
-#define NINPUTS 1
+#define NINPUTS 1 
 #define NOUTPUTS 1
-#define INPUT_UPPERBOUND (__plant_precisiont)1
-#define INPUT_LOWERBOUND (__plant_precisiont)-1
-const __plant_typet _controller_A[NSTATES][NSTATES] = {{ interval(1.00003),interval(-0.00003)},
-                                                        {interval (1),interval(0) }};
-const __plant_typet _controller_B[NSTATES] = { interval(0.0078125), interval(0) };
+#define INPUT_UPPERBOUND (__plant_precisiont)10000
+#define INPUT_LOWERBOUND (__plant_precisiont)-10000
+const __plant_typet _controller_A[NSTATES][NSTATES] = {{ interval(2.979234e+00),  interval(-1.479232e+00),  interval(9.792190e-01)},
+{ interval(2),  interval(0),  interval(0)},
+{ interval(0),  interval(5.000000e-01),  interval(0)}};
+const __plant_typet _controller_B[NSTATES] = {interval(1), interval(0), interval(0)};
+const __plant_typet __char_poly_const[4][4] = 
+ {{ 1,  0,  0,  0},
+{ -2.979234e+00,  1,  0,  0},
+{ 2.958465e+00,  0,  2,  0},
+{ -9.792190e-01,  0,  0,  1}};
 
-
-
-#endif /* BENCHMARK_H_ */
+#endif /*BENCHMARK_H_*/
