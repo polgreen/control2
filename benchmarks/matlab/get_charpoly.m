f2e60532f61d2fe59616bbf9daed2a7bc24b0893
polgreen <elizabeth.polgreen@cs.ox.ac.uk>
2018-12-13 10:30:43 +0000

    change capitalisation

--- benchmarks/matlab/get_charpoly.m
+++ benchmarks/matlab/get_charpoly.m
@@ -1,9 +1,9 @@
 function res=get_charpoly(n)
 
 
-A = sym('a',[n,n]);
-B = sym('b',[n,1]);
-K = sym('k',[1,n]);
+A = sym('A',[n,n]);
+B = sym('B',[n,1]);
+K = sym('K',[1,n]);
 I = eye(n);
 x = sym('x');
 
